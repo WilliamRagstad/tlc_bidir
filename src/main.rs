239c3992967ceb0373b5398ca04df80436c42d00
William Rågstad <william.ragstad@gmail.com>
2025-07-05 01:05:04 +0200

    Move repl

--- src/main.rs
+++ src/main.rs
@@ -38,76 +38,7 @@ fn main() {
             PRINT_OUT,
         );
     } else {
-        use std::io::Write;
-        loop {
-            print!("> ");
-            std::io::stdout().flush().unwrap();
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            let args: Vec<&str> = input.trim().split(' ').collect::<Vec<&str>>();
-            match *args.first().unwrap_or(&"") {
-                ":q" | ":quit" => break,
-                ":cls" | ":clear" => {
-                    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-                    continue;
-                }
-                ":env" => {
-                    if args.len() == 2 && args[1] == "clear" {
-                        env.clear();
-                    } else {
-                        for (name, term) in &env {
-                            println!("{} = {}", name, print::term(term));
-                        }
-                    }
-                    continue;
-                }
-                ":std" => {
-                    eval_prog(
-                        include_str!("./std.lc").into(),
-                        &mut env,
-                        verbose,
-                        PRINT_OUT,
-                    );
-                    continue;
-                }
-                ":load" => {
-                    let Some(file) = args.get(1) else {
-                        eprintln!("Usage: :load <file>");
-                        continue;
-                    };
-                    if let std::io::Result::Ok(content) = std::fs::read_to_string(file) {
-                        eval_prog(content, &mut env, verbose, PRINT_OUT);
-                    } else {
-                        eprintln!("Error reading file");
-                    }
-                    continue;
-                }
-                ":dbg" => {
-                    // Step through the program evaluation
-                    let input = args[1..].join(" ");
-                    eval_prog(input, &mut env, verbose, PRINT_DBG);
-                    continue;
-                }
-                ":help" => {
-                    println!("Commands:");
-                    println!("  :q, :quit      Quit the program");
-                    println!("  :cls, :clear   Clear the screen");
-                    println!("  :env           Print the current environment");
-                    println!("  :env clear     Clear the current environment");
-                    println!("  :load <file>   Load a file into the environment");
-                    println!("  :std           Load the standard library");
-                    println!("  :dbg <prog>    Step through the evaluation");
-                    println!("  :help          Print this help message");
-                    continue;
-                }
-                cmd if cmd.starts_with(":") => {
-                    eprintln!("Unknown command: {}, try :help", cmd);
-                    continue;
-                }
-                _ => {}
-            }
-            eval_prog(input, &mut env, verbose, PRINT_OUT);
-        }
+        repl(&mut env, verbose)
     }
 }
 
@@ -132,4 +63,72 @@ fn expr(args: &[String], verbose: bool) {
     let expr = args[2..].join(" ");
     let mut env = Env::new();
     eval_prog(expr, &mut env, verbose, PRINT_OUT);
+}
+
+fn repl(env: &mut Env, verbose: bool) {
+    use std::io::Write;
+    loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let args: Vec<&str> = input.trim().split(' ').collect::<Vec<&str>>();
+        match *args.first().unwrap_or(&"") {
+            ":q" | ":quit" => break,
+            ":cls" | ":clear" => {
+                print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+                continue;
+            }
+            ":env" => {
+                if args.len() == 2 && args[1] == "clear" {
+                    env.clear();
+                } else {
+                    for (name, term) in env.iter() {
+                        println!("{} = {}", name, print::term(term));
+                    }
+                }
+                continue;
+            }
+            ":std" => {
+                eval_prog(include_str!("./std.lc").into(), env, verbose, PRINT_OUT);
+                continue;
+            }
+            ":load" => {
+                let Some(file) = args.get(1) else {
+                    eprintln!("Usage: :load <file>");
+                    continue;
+                };
+                if let std::io::Result::Ok(content) = std::fs::read_to_string(file) {
+                    eval_prog(content, env, verbose, PRINT_OUT);
+                } else {
+                    eprintln!("Error reading file");
+                }
+                continue;
+            }
+            ":dbg" => {
+                // Step through the program evaluation
+                let input = args[1..].join(" ");
+                eval_prog(input, env, verbose, PRINT_DBG);
+                continue;
+            }
+            ":help" => {
+                println!("Commands:");
+                println!("  :q, :quit      Quit the program");
+                println!("  :cls, :clear   Clear the screen");
+                println!("  :env           Print the current environment");
+                println!("  :env clear     Clear the current environment");
+                println!("  :load <file>   Load a file into the environment");
+                println!("  :std           Load the standard library");
+                println!("  :dbg <prog>    Step through the evaluation");
+                println!("  :help          Print this help message");
+                continue;
+            }
+            cmd if cmd.starts_with(":") => {
+                eprintln!("Unknown command: {}, try :help", cmd);
+                continue;
+            }
+            _ => {}
+        }
+        eval_prog(input, env, verbose, PRINT_OUT);
+    }
 }
